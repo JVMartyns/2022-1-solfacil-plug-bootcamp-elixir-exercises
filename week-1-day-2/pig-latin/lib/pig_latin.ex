9414248e7b86eef1110d37afd8ee275dee946ca2
JVMartyns <jvmartyns2015@gmail.com>
2022-03-13 20:49:32 -0300

    Update pig_latin.ex

--- week-1-day-2/pig-latin/lib/pig_latin.ex
+++ week-1-day-2/pig-latin/lib/pig_latin.ex
@@ -4,46 +4,64 @@ defmodule PigLatin do
   """
   @spec translate(phrase :: String.t()) :: String.t()
   def translate(phrase) do
-    vowels = "aeiou"
-    consonants = "bcdfghjklmnpqrstvwxyz"
-    list_phrase = String.split(String.trim(phrase))
+    vowels = String.split("a,e,i,o,u", ",")
+    consonants = String.split("b,c,d,f,g,h,j,k,l,m,n,p,q,r,s,t,v,w,x,y,z", ",")
+    list_of_words = String.split(String.trim(phrase))
 
-    Enum.map(list_phrase, fn word ->
+    Enum.map(list_of_words, fn word ->
       cond do
-        # word beginning with [a,e,i,o,u] or beginning with "y" and following a consonant
-        String.at(word, 0) in String.split(vowels, "") or
-            (String.at(word, 0) == "y" and String.at(word, 1) in String.split(consonants, "")) ->
+        # "word beginning with [a,e,i,o,u]" or
+        # "word beginning with q without a following u" or
+        # "word beginning with "y" and following a consonant or
+        # "word beginning with [xr, xb]"
+        String.at(word, 0) in vowels or
+          (String.at(word, 0) in vowels and String.slice(word, 1..2) == "qu") or
+          (String.at(word, 0) == "y" and String.at(word, 1) in consonants) or
+            String.slice(word, 0..1) in ["xr", "xb"] ->
           word <> "ay"
 
-        # word beginning with sch or thr
-        String.slice(word, 0..2) == "sch" or
-            String.slice(word, 0..2) == "thr" ->
+        # "word beginning with sch or thr" or
+        # "word beginning with qu and a preceding consonant"
+        String.slice(word, 0..2) in ["sch", "thr"] or
+            (String.at(word, 0) in consonants and String.slice(word, 1..2) == "qu") ->
           String.slice(word, 3..-1) <> String.slice(word, 0..2) <> "ay"
 
         # word beginning with th or qu or ch
-        String.slice(word, 0..1) == "th" or
-          String.slice(word, 0..1) == "qu" or
-            String.slice(word, 0..1) == "ch" ->
+        String.slice(word, 0..1) in ["th", "qu", "ch"] ->
           String.slice(word, 2..-1) <> String.slice(word, 0..1) <> "ay"
 
-        String.at(word, 0) in String.split(consonants, "") and String.slice(word, 1..2) == "qu" ->
-          String.slice(word, 3..-1) <> String.slice(word, 0..2) <> "ay"
+        # "y is treated like a consonant at the beginning of a word"
+        # "y as second letter in two letter word"
+        # "word beginning with one consoant"
+        (String.length(word) == 2 and String.at(word, 1) == "y") or
+            String.at(word, 0) == "y" ->
+          String.slice(word, 1..-1) <> String.at(word, 0) <> "ay"
 
-        # word beginning with three consonants
-        String.at(word, 0) in String.split(consonants, "") and
-          String.at(word, 1) in String.split(consonants, "") and
-            String.at(word, 2) in String.split(consonants, "") ->
-          String.slice(word, 3..-1) <> String.slice(word, 0..2) <> "ay"
+        # "y is treated like a vowel at the end of a consonant cluster"
 
-        # word beginning with two consonants
-        String.at(word, 0) in String.split(consonants, "") and
-            String.at(word, 1) in String.split(consonants, "") ->
-          String.slice(word, 2..-1) <> String.slice(word, 0..1) <> "ay"
 
-        String.at(word, 0) in String.split(consonants, "") ->
-          String.slice(word, 1..-1) <> String.slice(word, 0..0) <> "ay"
+        # "word beginning with two consonants"
+        #"word beginning with three consonants"
+        #"word beginning with a series of consonants : aliens speak Pig Latin too"
+        String.at(word, 0) in consonants ->
+          String.slice(word, teste(word, vowels)..-1) <>
+            String.slice(word, 0..(teste(word, vowels) - 1)) <> "ay"
       end
     end)
     |> Enum.join(" ")
   end
+
+  def teste(word, vowels) do
+    vogais =
+    palavra = String.split(word, "", trim: true)
+
+    Enum.reduce_while(palavra, 0, fn letra, acc ->
+      if letra in vowels do
+        {:halt, acc}
+      else
+        {:cont, acc + 1}
+      end
+    end)
+  end
+
 end
--- week-1-day-2/pig-latin/test/pig_latin_test.exs
+++ week-1-day-2/pig-latin/test/pig_latin_test.exs
@@ -59,12 +59,12 @@ defmodule PigLatinTest do
       assert PigLatin.translate("pleasure") == "easureplay"
     end
 
-    # @tag :pending
+    #@tag :pending
     test "word beginning with three consonants" do
       assert PigLatin.translate("stringify") == "ingifystray"
     end
 
-    @tag :pending
+    #@tag :pending
     test "word beginning with a series of consonants : aliens speak Pig Latin too" do
       assert PigLatin.translate("zkrrkrkrkrzzzkewk") == "ewkzkrrkrkrkrzzzkay"
     end
@@ -108,41 +108,41 @@ defmodule PigLatinTest do
       assert PigLatin.translate("yttria") == "yttriaay"
     end
 
-    @tag :pending
+    # @tag :pending
     test "word beginning with y, followed by another consonant" do
       assert PigLatin.translate("yddria") == "yddriaay"
     end
 
-    @tag :pending
+    # @tag :pending
     test "word beginning with xr" do
       assert PigLatin.translate("xray") == "xrayay"
     end
 
-    @tag :pending
+    # @tag :pending
     test "word beginning with xb" do
       assert PigLatin.translate("xbot") == "xbotay"
     end
   end
 
   describe "position of y in a word determines if it is a consonant or a vowel" do
-    @tag :pending
+    # @tag :pending
     test "y is treated like a consonant at the beginning of a word" do
       assert PigLatin.translate("yellow") == "ellowyay"
     end
 
-    @tag :pending
+    #@tag :pending
     test "y is treated like a vowel at the end of a consonant cluster" do
       assert PigLatin.translate("rhythm") == "ythmrhay"
     end
 
-    @tag :pending
+    # @tag :pending
     test "y as second letter in two letter word" do
       assert PigLatin.translate("my") == "ymay"
     end
   end
 
   describe "phrases are translated" do
-    @tag :pending
+    # @tag :pending
     test "a whole phrase" do
       assert PigLatin.translate("quick fast run") == "ickquay astfay unray"
     end
